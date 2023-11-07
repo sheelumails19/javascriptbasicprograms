b37b64d31ffebf85902fab2c693754cf59f5d57b
Sri <rajamails19@gmail.com>
2023-11-07 09:09:16 -0800

    Create Algo copy.js

---
+++ Algo copy.js
@@ -0,0 +1,15 @@
+// JavaScript program to traverse an array
+
+// Define an array
+let arr = [1, 2, 3, 4];
+
+// Get the length of the array
+let N = arr.length;
+
+// Traverse the elements of the array
+for (let i = 0; i < N; i++) {
+    // Print the element
+    console.log(arr[i]);
+}
+
+// Output :- 1 2 3 4
